37a48d9f067152b895bdfcf3120ea9c8b8d20f9d
alekvan <aleksandar.vangelov@gmail.com>
2022-04-23 22:51:47 +0200

    TO DO DELETE BUTTON

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -6,15 +6,10 @@ import 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBYferIOKCmXP2TOgIpwXOxehuuQp99MnE',
-
   authDomain: 'firegram-ee5bd.firebaseapp.com',
-
   projectId: 'firegram-ee5bd',
-
   storageBucket: 'firegram-ee5bd.appspot.com',
-
   messagingSenderId: '653157955184',
-
   appId: '1:653157955184:web:8cbc78a29babb53dd7e6b5',
 };
 
@@ -24,5 +19,6 @@ firebase.initializeApp(firebaseConfig);
 
 const projectStorage = firebase.storage();
 const projectFirestore = firebase.firestore();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
 
-export { projectStorage, projectFirestore };
+export { projectStorage, projectFirestore, timestamp };
--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { projectStorage } from '../firebase/config';
+import { projectStorage, projectFirestore, timestamp } from '../firebase/config';
 
 const useStorage = (file) => {
   const [progress, setProgress] = useState(0);
@@ -7,25 +7,24 @@ const useStorage = (file) => {
   const [url, setUrl] = useState(null);
 
   useEffect(() => {
+    // references
     const storageRef = projectStorage.ref(file.name);
-
-    storageRef.put(file).on(
-      'state_changed',
-      (snap) => {
-        let percantage = (snap.bytesTransferred / snap.totalBytes) * 100;
-        setProgress(percantage);
-      },
-      (err) => {
-        setError(err);
-      },
-      async () => {
-        const url = storageRef.getDownloadURL();
-        setUrl(url);
-      }
-    );
+    const collectionRef = projectFirestore.collection('images');
+    
+    storageRef.put(file).on('state_changed', (snap) => {
+      let percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
+      setProgress(percentage);
+    }, (err) => {
+      setError(err);
+    }, async () => {
+      const url = await storageRef.getDownloadURL();
+      const createdAt = timestamp();
+      await collectionRef.add({ url, createdAt });
+      setUrl(url);
+    });
   }, [file]);
 
-  return { progress, error, url };
-};
+  return { progress, url, error };
+}
 
 export default useStorage;
